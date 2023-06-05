b3eabd6c023e27e2e5db22f76276406c90bc7eb7
CheyenneMDavid <>
2023-06-05 02:12:08 +0000

    add mouseenter, mouseleave with red/blue change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-// If I use the p element, when the paragraphs are clicked on, they both disapear at the same time.
-// by using the "this" keyword, the paragraphs can be clicked on individually
-$('p').click(function() {
-    $(this).slideToggle('slow');
+$("button").mouseenter(function() {
+    $(this).removeClass("makeRed").addClass("makeBlue");
+});
+
+
+$("button").mouseleave(function(){
+    $(this).removeClass("makeBlue").addClass("makeRed");
 });
