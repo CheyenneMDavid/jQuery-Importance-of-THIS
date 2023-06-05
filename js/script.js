f44958e2d61961734b7db898c2b6d980d0e603fe
CheyenneMDavid <>
2023-06-05 01:55:41 +0000

    Add all files to the commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// If I use the p element, when the paragraphs are clicked on, they both disapear at the same time.
+// by using the "this" keyword, the paragraphs can be clicked on individually
 $('p').click(function() {
     $(this).slideToggle('slow');
 });
